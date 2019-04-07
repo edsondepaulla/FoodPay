df37af9ef75331327c536360fb335afa5a717115
Edson de Paula <edsondepaulla@gmail.com>
2019-04-07 12:27:59 -0300

    a

--- www/js/functions.js
+++ www/js/functions.js
@@ -172,10 +172,35 @@ function onErrorUser(_this){
 }
 
 $(document).ready(function() {
-    document.addEventListener("deviceready", function(){
-        QRScanner.prepare(onDone); // show the prompt
+    $('#scan_qrcode').click(function () {
+        $('body').attr('scan_qrcode', 1);
+        try {
+            QRScanner.scan(function (err, text) {
+                if (err) {
 
-        function onDone(err, status){
+                } else {
+                    Factory.ajax(
+                        {
+                            action: 'qrcode/get',
+                            data: {
+                                TEXT: text
+                            }
+                        },
+                        function (data) {
+                            if (data.status == 1)
+                                window.location = data.url;
+                            else
+                                window.location = '#!/';
+                        }
+                    );
+                }
+            });
+            QRScanner.show();
+        } catch (err) {
+        }
+    });
+    try {
+        QRScanner.prepare(function (err, status) {
             if (err) {
                 // here we can handle errors and clean up any loose ends.
                 console.error(err);
@@ -192,68 +217,7 @@ $(document).ready(function() {
                 // Android, a denial isn't permanent unless the user checks the "Don't
                 // ask again" box.) We can ask again at the next relevant opportunity.
             }
-        }
-
-        $('#scan_qrcode').click(function () {
-            $('html').attr('ativar', '1');
-            $('#map').remove();
-
-            // Start a scan. Scanning will continue until something is detected or
-// `QRScanner.cancelScan()` is called.
-            QRScanner.scan(displayContents);
-
-            function displayContents(err, text){
-                if(err){
-                    // an error occurred, or the scan was canceled (error code `6`)
-                } else {
-                    // The scan completed, display the contents of the QR code:
-                    alert(text);
-                }
-            }
-
-// Make the webview transparent so the video preview is visible behind it.
-            QRScanner.show();
-// Be sure to make any opaque HTML elements transparent here to avoid
-// covering the video.
-
-
-
-            /*cordova.plugins.barcodeScanner.scan(
-                function (result) {
-                    if (!result.cancelled) {
-                        Factory.ajax(
-                            {
-                                action: 'qrcode/get',
-                                data: {
-                                    TEXT: result.text
-                                }
-                            },
-                            function (data) {
-                                if(data.status == 1)
-                                    window.location = data.url;
-                                else
-                                    window.location = '#!/';
-                            }
-                        );
-                    }
-                },
-                function (error) {
-                    alert("Scanning failed: " + error);
-                },
-                {
-                    preferFrontCamera : false, // iOS and Android
-                    showFlipCameraButton : false,// iOS and Android
-                    showTorchButton : true,// iOS and Android
-                    torchOn: false, // Android, launch with the torch switched on (if available)
-                    saveHistory: false, // Android, save scan history (default false)
-                    prompt: "", // Android
-                    resultDisplayDuration: 0, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-                    formats : "QR_CODE",// default: all but PDF_417 and RSS_EXPANDED
-                    orientation : "portrait",// Android only (portrait|landscape), default unset so it rotates with the device
-                    disableAnimations : false, // iOS
-                    disableSuccessBeep: false // iOS and Android
-                }
-            );*/
         });
-    }, false);
+    } catch (err) {
+    }
 });
