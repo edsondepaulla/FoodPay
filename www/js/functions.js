fa54d9876b3b298a1ec14adf65a70a67d490192c
Edson de Paula <edsondepaulla@gmail.com>
2019-04-07 11:43:35 -0300

    a

--- www/js/functions.js
+++ www/js/functions.js
@@ -173,8 +173,49 @@ function onErrorUser(_this){
 
 $(document).ready(function() {
     document.addEventListener("deviceready", function(){
+        QRScanner.prepare(onDone); // show the prompt
+
+        function onDone(err, status){
+            if (err) {
+                // here we can handle errors and clean up any loose ends.
+                console.error(err);
+            }
+            if (status.authorized) {
+                // W00t, you have camera access and the scanner is initialized.
+                // QRscanner.show() should feel very fast.
+            } else if (status.denied) {
+                // The video preview will remain black, and scanning is disabled. We can
+                // try to ask the user to change their mind, but we'll have to send them
+                // to their device settings with `QRScanner.openSettings()`.
+            } else {
+                // we didn't get permission, but we didn't get permanently denied. (On
+                // Android, a denial isn't permanent unless the user checks the "Don't
+                // ask again" box.) We can ask again at the next relevant opportunity.
+            }
+        }
+
         $('#scan_qrcode').click(function () {
-            cordova.plugins.barcodeScanner.scan(
+            // Start a scan. Scanning will continue until something is detected or
+// `QRScanner.cancelScan()` is called.
+            QRScanner.scan(displayContents);
+
+            function displayContents(err, text){
+                if(err){
+                    // an error occurred, or the scan was canceled (error code `6`)
+                } else {
+                    // The scan completed, display the contents of the QR code:
+                    alert(text);
+                }
+            }
+
+// Make the webview transparent so the video preview is visible behind it.
+            QRScanner.show();
+// Be sure to make any opaque HTML elements transparent here to avoid
+// covering the video.
+
+
+            
+            /*cordova.plugins.barcodeScanner.scan(
                 function (result) {
                     if (!result.cancelled) {
                         Factory.ajax(
@@ -209,10 +250,7 @@ $(document).ready(function() {
                     disableAnimations : false, // iOS
                     disableSuccessBeep: false // iOS and Android
                 }
-            );
-            setTimeout(function(){
-                window.open('teste.html', '_blank', 'location=yes');
-            }, 1000);
+            );*/
         });
     }, false);
 });
